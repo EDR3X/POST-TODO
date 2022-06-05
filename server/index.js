841cd4c2e2f7abc805f01218c18c9643799f4e24
EDR3X <dhunganaannanta@gmail.com>
2022-06-05 16:29:10 +0545

    PostgreSQL pool init

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
-const cors = require("cos");
 const PORT = 5000;
+const cors = require("cos");
+const pool = require("./db");
 
 //*middleware
 app.use(cors());
 app.use(express.json());
+
+//* ROUTES
+
+//*create a todo
+
+//* get all todos
+
+//* update a todo
+
+//* delete a todo
 
 app.listen(PORT, () => console.log(`server started at port ${PORT}`));
